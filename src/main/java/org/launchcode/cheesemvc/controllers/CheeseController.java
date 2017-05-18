e61c8cea7f57b975f624b14603d43f7e56cd6a58
AndrewScalise <paradigmcomplex@gmail.com>
2017-05-18 12:41:49 -0500

    Remove from list working

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,6 +1,6 @@
 package org.launchcode.cheesemvc.controllers;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Mugen on 5/18/2017.
@@ -17,7 +18,7 @@ import java.util.ArrayList;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String,String> cheeses = new HashMap<>();
 
     // Request path: /cheese
     @RequestMapping(value="")
@@ -35,10 +36,29 @@ public class CheeseController {
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String description){
+        cheeses.put(cheeseName, description);
 
         //Redirect to /cheese
+        return "redirect:";
+    }
+
+    @RequestMapping(value="remove", method=RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model){
+        model.addAttribute("title", "Remove Cheese");
+        model.addAttribute("cheeses", cheeses);
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value="remove", method=RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam ArrayList<String> cheesed){
+
+        for(int i = 0; i < cheesed.size(); i++){
+            String checked = cheesed.get(i);
+            if(cheeses.containsKey(checked)){
+                cheeses.remove(checked);
+            }
+        }
         return "redirect:";
     }
 }
